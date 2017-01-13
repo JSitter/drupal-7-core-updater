46f9c47a1ef97674a4697c59500228af53710566
Justin <justin@jaytria.com>
2017-01-13 03:41:21 -0800

    first test

--- d7Update.py
+++ d7Update.py
@@ -4,7 +4,7 @@ import os
 import sys
 from os.path import expanduser
 home = expanduser("~")
-
+updaterDir = home + "/pyD7Updater"
 
 #######################################3
 #	Check for Valid Flags
@@ -16,5 +16,10 @@ else:
 	error = "Unable to comply. File URL not specified."
 	print(error)
 	exit()
+#Check if upload directory has been created
+if not os.path.isdir(updaterDir):
+	os.makedirs(updaterDir)
+
+
 
 
