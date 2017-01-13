1d3e1d07a03d6b9e8ed1022a7a27445eb7a46836
Justin <justin@jaytria.com>
2017-01-13 07:48:05 -0800

    unzippening

--- d7Update.py
+++ d7Update.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 
+import zipfile
 import os
 import sys
 from os.path import expanduser
 home = expanduser("~")
-updaterDir = home + "/pyD7Updater"
+
+updaterDir = home + "/pyD7Updater/"
+saveLoc = updaterDir + "d7.zip"
+
+import urllib.request
 
 #######################################3
 #	Check for Valid Flags
@@ -19,7 +24,15 @@ else:
 #Check if upload directory has been created
 if not os.path.isdir(updaterDir):
 	os.makedirs(updaterDir)
+	os.makedirs(updaterDir + "/extracts/")
 
+#download zip file
+urllib.request.urlretrieve(downloadLink, saveLoc)
 
+zipRef = zipfile.ZipFile(saveLoc, 'r') 
+print(zipfile.ZipFile.printdir)
+zipRef.extractall(updaterDir + "/extracts/")
+#print(zipfile.ZipFile.namelist())
+zipRef.close()
 
 
